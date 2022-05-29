e681ef8345015105131f4dd8058dcbfeaf7d9204
JiaQi <JiaQi@126.com>
2022-05-29 14:26:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author JQRM
+ * @create 2022-05-29-14:21
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
